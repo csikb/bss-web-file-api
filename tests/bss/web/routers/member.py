540e224b85c04e2986fdcc21e89f913bbbcc3490
Bence Csik <22349790+csikb@users.noreply.github.com>
2022-11-28 15:41:43 +0000

    add lint

--- tests/bss/web/routers/member.py
+++ tests/bss/web/routers/member.py
@@ -8,9 +8,6 @@ client = TestClient(router)
 
 
 def test_create_member_folder(mocker):
-    video = {
-        "id": "01234567-0123-0123-0123-0123456789ab",
-        "url": "url"
-    }
+    video = {"id": "01234567-0123-0123-0123-0123456789ab", "url": "url"}
     response = client.post("/api/v1/member", json=video)
     assert response.json() == video
