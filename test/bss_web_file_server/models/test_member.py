a94a0d3093d153eaa108060a8eaafb1e2458a6bd
Bence Csik <22349790+csikb@users.noreply.github.com>
2022-12-02 13:31:41 +0000

    add docker

--- test/bss_web_file_server/models/test_member.py
+++ test/bss_web_file_server/models/test_member.py
@@ -5,7 +5,7 @@ from bss_web_file_server.models.member import Member
 
 def test_should_create_member():
     url = "url"
-    id = UUID("{12345678-1234-5678-1234-567812345678}")
-    member = Member(id=id, url=url)
-    assert member.id is id
+    uid = UUID("{12345678-1234-5678-1234-567812345678}")
+    member = Member(id=uid, url=url)
+    assert member.id is uid
     assert member.url is url
